cb3b19eff60781db649f9d2cb33e79d8c04c9c03
vinicius-hrq <vinicius19h@gmail.com>
2021-08-03 16:07:24 -0300

    Adicionando back-end remoto

---
+++ day1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "west"
+  region = "us-west-2"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "vinicius-hrq-terraform"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
