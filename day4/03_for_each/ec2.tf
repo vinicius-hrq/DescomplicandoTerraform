cf60ebd199829313551c5764b306ee5295d8f53c
vinicius-hrq <vinicius19h@gmail.com>
2021-09-15 11:25:49 -0300

    for each com variaveis

---
+++ day4/03_for_each/ec2.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  for_each      = toset(var.instance_type)
+  instance_type = each.value
+}
