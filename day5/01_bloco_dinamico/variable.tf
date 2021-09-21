598c3fb171037cd9755a09dbfc950e735f3b5e87
vinicius-hrq <vinicius19h@gmail.com>
2021-09-21 09:35:02 -0300

    Variable blocks - list of EBS block

---
+++ day5/01_bloco_dinamico/variable.tf
@@ -0,0 +1,8 @@
+variable "blocks" {
+    type = list(object({
+        device_name = string
+        volume_size = string
+        volume_type = string
+    }))
+    description = "List of EBS Block"  
+}
